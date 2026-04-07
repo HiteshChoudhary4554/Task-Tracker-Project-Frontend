651cc44bf4bd3e1b2b3cb9e7c252a44e625e98c6
Hitesh Choudhary <hk2310756@gmail.com>
2026-04-08 01:09:23 +0530

    update 0.5

--- src/modules/task/components/TaskCardList.jsx
+++ src/modules/task/components/TaskCardList.jsx
@@ -1,27 +1,19 @@
-import React, { useEffect, useState } from "react";
-
 function TaskCardList({ date = "", title, onClick }) {
-  const [expire, setExpire] = useState(null);
-  useEffect(() => {
-    function fetchDate(date) {
-      const taskDate = new Date(date);
-      const today = new Date();
+  const taskDate = new Date(date);
+  const today = new Date();
 
-      taskDate.setHours(0, 0, 0, 0);
-      today.setHours(0, 0, 0, 0);
+  taskDate.setHours(0, 0, 0, 0);
+  today.setHours(0, 0, 0, 0);
 
-      const isExpired = taskDate >= today;
-      const expiren = isExpired ? "#d0f325" : "#e54a4a";
-      setExpire(expiren);
-    }
-    fetchDate(date);
-  }, [date]);
+  const isExpired = taskDate >= today;
+
   // bg-[#292a2e]
   // bg-[${expire}]
+  // isExpired ? "#d0f325" : "#e54a4a";
   return (
     <div
       onClick={onClick}
-      className={`w-full rounded-2xl  shadow border-gray-300 bg-[#d0f325] px-6 py-4`}
+      className={`w-full rounded-2xl  shadow border-gray-300 ${isExpired ? "bg-[#d0f325]" : "bg-[#e54a4a]"} px-6 py-4`}
     >
       <h2 className="text-[16px] font-semibold text-black">{title}</h2>
     </div>
